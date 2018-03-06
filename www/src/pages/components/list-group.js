19e9cd3648b14b1658f1b8dc157817e88b5458eb
Adrien Lacroix <oni.dev.box@gmail.com>
2018-03-06 23:01:48 +0100

    wip

--- www/src/pages/components/list-group.js
+++ www/src/pages/components/list-group.js
@@ -5,9 +5,10 @@ import LinkToSource from '../../components/LinkToSource';
 import PropTable from '../../components/PropTable';
 import ReactPlayground from '../../components/ReactPlayground';
 
-import ListGroupDefault from '../../examples/ListGroupDefault';
+import Basic from '../../examples/ListGroup/Basic';
+import Active from '../../examples/ListGroup/Active';
+import Disabled from '../../examples/ListGroup/Disabled';
 import ListGroupLinked from '../../examples/ListGroupLinked';
-import ListGroupActive from '../../examples/ListGroupActive';
 import ListGroupStyle from '../../examples/ListGroupStyle';
 import ListGroupHeader from '../../examples/ListGroupHeader';
 import ListGroupCustom from '../../examples/ListGroupCustom';
@@ -21,14 +22,44 @@ export default function ListGroupSection({ data }) {
       </h2>
 
       <p>
-        List groups are a flexible and powerful component for displaying not
-        only simple lists of elements, but complex ones with custom content.
+        List groups are a flexible and powerful component for displaying a
+        series of content. Modify and extend them to support just about any
+        content within.
       </p>
 
       <h3>
-        <Anchor id="listgroup-default">Centers by default</Anchor>
+        <Anchor id="listgroup-basic">Basic example</Anchor>
       </h3>
-      <ReactPlayground codeText={ListGroupDefault} />
+      <p>
+        The most basic list group is an unordered list with list items and the
+        proper classes. Build upon it with the options that follow, or with your
+        own CSS as needed.
+      </p>
+
+      <ReactPlayground codeText={Basic} />
+
+      <h3>
+        <Anchor id="listgroup-active">Active items</Anchor>
+      </h3>
+      <p>
+        Add the <code>active</code> prop to a{' '}
+        <code>&lt;ListGroupItem /&gt;</code> to indicate the current active
+        selection.
+      </p>
+
+      <ReactPlayground codeText={Active} />
+
+      <h3>
+        <Anchor id="listgroup-disabled">Disabled items</Anchor>
+      </h3>
+      <p>
+        Add the <code>disabled</code> prop to a{' '}
+        <code>&lt;ListGroupItem /&gt;</code> to make it appear disabled. Note
+        that some elements with .disabled will also require custom JavaScript to
+        fully disable their click events (e.g., links).
+      </p>
+
+      <ReactPlayground codeText={Disabled} />
 
       <h3>
         <Anchor id="listgroup-linked">Linked</Anchor>
@@ -38,15 +69,6 @@ export default function ListGroupSection({ data }) {
         <code>ListGroupItem</code>, to create a linked or clickable element.
       </p>
       <ReactPlayground codeText={ListGroupLinked} />
-
-      <h3>
-        <Anchor id="listgroup-styling-state">Styling by state</Anchor>
-      </h3>
-      <p>
-        Set the <code>active</code> or <code>disabled</code> prop to{' '}
-        <code>true</code> to mark or disable the item.
-      </p>
-      <ReactPlayground codeText={ListGroupActive} />
 
       <h3>
         <Anchor id="listgroup-styling-color">Styling by color</Anchor>
